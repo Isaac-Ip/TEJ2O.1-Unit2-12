1c6ae03b6b01c3b514e9a71c2c30f4d700aea7c1
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-10-15 22:40:43 -0400

    if else statements with hardware

--- main.ts
+++ main.ts
@@ -1,8 +1,46 @@
-/* Copyright (c) 2020 MTHS All rights reserved
+/* Copyright (c) 2025 Isaac Ip All rights reserved
  *
- * Created by: XXX
- * Created on: Sep 2020
- * This program ...
+ * Created by: Isaac Ip
+ * Created on: Oct 2025
+ * This program uses if, else statements with hardware.
 */
 
-basic.showString('Hello, World!')
+// variables
+let distanceToObject: number = 0
+let neopixelStrip: neopixel.Strip = null
+
+//cleanup
+basic.clearScreen()
+neopixelStrip = neopixel.create(DigitalPin.P16, 4, NeoPixelMode.RGB)
+neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Black))
+neopixelStrip.show()
+basic.showIcon(IconNames.Happy)
+
+input.onButtonPressed(Button.A, function () {
+    // get distance
+    distanceToObject = sonar.ping(
+        DigitalPin.P0,
+        DigitalPin.P1,
+        PingUnit.Centimeters
+    )
+    basic.clearScreen()
+    if (distanceToObject > 10) {
+        // if distance is less than to 10 cm
+        basic.showIcon(IconNames.No)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Red))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Red))
+    } else {
+        // if distance is more than or equal to 10 cm
+        basic.showIcon(IconNames.Yes)
+        neopixelStrip.setPixelColor(0, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(1, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(2, neopixel.colors(NeoPixelColors.Green))
+        neopixelStrip.setPixelColor(3, neopixel.colors(NeoPixelColors.Green))
+    }
+    neopixelStrip.show()
+})
