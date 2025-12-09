bcdcd5bf2f506ccdf76ce396f05e74055d4714ba
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-12-08 23:04:56 -0500

    test

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -8,48 +8,12 @@ This program uses if, else statements with hardware.
 from microbit import *
 import neopixel
 
-class HCSR04:
-    # this class abstracts out the functionality of the HC-SR04 and
-    #   returns distance in mm
-    # Trig: pin 1
-    # Echo: pin 2
-    def __init__(self, tpin=pin1, epin=pin2, spin=pin13):
-        self.trigger_pin = tpin
-        self.echo_pin = epin
-        self.sclk_pin = spin
-
-    def distance_mm(self):
-        spi.init(baudrate=125000, sclk=self.sclk_pin,
-            mosi=self.trigger_pin, miso=self.echo_pin)
-        pre = 0
-        post = 0
-        k = -1
-        length = 500
-        resp = bytearray(length)
-        resp[0] = 0xFF
-        spi.write_readinto(resp, resp)
-        # find first non zero value
-        try:
-            i, value = next((ind, v) for ind, v in enumerate(resp) if v)
-        except StopIteration:
-            i = -1
-        if i > 0:
-            pre = bin(value).count("1")
-            # find first non full high value afterwards
-            try:
-                k, value = next((ind, v)
-                    for ind, v in enumerate(resp[i:length - 2]) if resp[i + ind + 1] == 0)
-                post = bin(value).count("1") if k else 0
-                k = k + i
-            except StopIteration:
-                i = -1
-        dist= -1 if i < 0 else round(((pre + (k - i) * 8. + post) * 8 * 0.172) / 2)
-        return dist
-
+from hcsr04 import HCSR04
 
 
 # variables
-distanceToObject = 0
+sensor = HCSR04(trigger_pin=12, echo_pin=13)
+distance = sensor.distance_cm()
 neopixelStrip = neopixel.NeoPixel(pin16, 4)
 
 # cleanup
