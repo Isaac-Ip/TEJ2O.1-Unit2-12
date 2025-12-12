7e43443fd81d6f27ccd3ad380ffa356df4cc6049
Isaac-Ip <isaac.ip@stu.ocsb.ca>
2025-12-12 22:49:58 +0000

    completed python

--- MicroPython/main.py
+++ MicroPython/main.py
@@ -1,17 +1,19 @@
 """
 Created by: Isaac Ip
-Created on: Oct 2025
+Created on: Dec 2025
 This module is a Micro:bit MicroPython program
 This program uses if, else statements with hardware.
 """
 
 from microbit import *
+from machine import time_pulse_us
 import neopixel
-from hcsr04 import HCSR04
 
-# variables
-sonar = HCSR04(trigger_pin=12, echo_pin=13)
-distanceToObject = sonar.distance_mm() / 10
+# variables and setup
+trig = pin12
+echo = pin13
+trig.write_digital(0)
+echo.read_digital()
 neopixelStrip = neopixel.NeoPixel(pin16, 4)
 
 # cleanup
@@ -25,9 +27,17 @@ display.show(Image.HAPPY)
 
 while True:
     if button_a.is_pressed():
-        distanceToObject = sonar.distance_mm() / 10
-        display.clear()
-        if distanceToObject < 10:
+        # Output a pulse to trigger ultrasonic burst
+        trig.write_digital(1)
+        trig.write_digital(0)
+
+        # Measure the input echo pulse in microseconds, convert to seconds
+        micros = time_pulse_us(echo, 1)
+        t_echo = micros / 1000000
+        # Calculate distance in cm and display on micro:bit
+        dist_cm = (t_echo / 2) * 34300
+        display.scroll(str(int(dist_cm)))
+        if dist_cm < 10:
             display.show(Image.NO)
             neopixelStrip[0] = (255, 0, 0)
             neopixelStrip[1] = (255, 0, 0)
@@ -39,3 +49,4 @@ while True:
             neopixelStrip[1] = (0, 128, 0)
             neopixelStrip[2] = (0, 128, 0)
             neopixelStrip[3] = (0, 128, 0)
+    neopixelStrip.show()
